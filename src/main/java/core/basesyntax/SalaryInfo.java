51848d4aef6afcea7b49d733e56b9c32e944bff9
Michal <galusmichal@outlook.com>
2026-05-18 00:08:03 +0200

    second solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -6,37 +6,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SalaryInfo {
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         List<String> correctDateList = getCorrectDateList(data, dateFrom, dateTo);
 
-        return getStringResult(names, correctDateList,  dateFrom, dateTo);
+        return getStringResult(names, correctDateList, dateFrom, dateTo);
     }
 
-    private String getStringResult (String[] names, List<String> correctDateList, String dateFrom, String dateTo) {
+    private String getStringResult(String[] names, List<String> correctDateList,
+                                    String dateFrom, String dateTo) {
         String salaryInfo = "Report for period " + dateFrom + " - " + dateTo;
-        for(String name : names) {
+        for (String name : names) {
             int salary = 0;
-            for(String dateElement : correctDateList) {
+            for (String dateElement : correctDateList) {
                 String[] recordsElement = dateElement.split(" ");
-                if(name.equals(recordsElement[1])) {
-                    salary += Integer.parseInt(recordsElement[2]) * Integer.parseInt(recordsElement[3]);
+                if (name.equals(recordsElement[1])) {
+                    salary +=
+                            Integer.parseInt(recordsElement[2]) * Integer.parseInt(recordsElement[3]);
                 }
             }
-            salaryInfo += "\n" + name + " - "  + salary;
+            salaryInfo += "\n" + name + " - " + salary;
         }
         return salaryInfo;
     }
 
     private List<String> getCorrectDateList (String[] data, String dateFrom, String dateTo) {
         List<String> resultList = new ArrayList<>();
-        for(String record : data) {
+        for (String record : data) {
             String[] dataElement = record.split(" ");
             LocalDate workDay = LocalDate.parse(dataElement[0], formatter);
             LocalDate startDate = LocalDate.parse(dateFrom, formatter);
             LocalDate endDate = LocalDate.parse(dateTo, formatter);
-            if(!workDay.isBefore(startDate) && !workDay.isAfter(endDate)) {
+            if (!workDay.isBefore(startDate) && !workDay.isAfter(endDate)) {
                 resultList.add(record);
             }
         }
